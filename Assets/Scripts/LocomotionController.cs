9f2abe7a66db2ebffcfed4d10e85c17b5cb91fbd
kanggeonnim <gun4329@gmail.com>
2023-06-12 21:50:55 +0900

    create: main sound

--- Assets/Scripts/LocomotionController.cs
+++ Assets/Scripts/LocomotionController.cs
@@ -16,9 +16,14 @@ public class LocomotionController : MonoBehaviour
 
     private void Update() {
         float value = teleportReference.action.ReadValue<float>();
-        if(value > 0.1f)
-        {
-            teleportRay.SetActive(true);
+        if(value > 0.1f) //&& EnableTeleport)
+        {   
+            // Vector3 pos; Vector3 norm; int index; bool validTarget;
+            // bool isInteractorRayHovering = rayInteractor.TryGetHitInfo(out pos, out norm, out index, out validTarget);
+            // if(!isInteractorRayHovering)
+            {
+                teleportRay.SetActive(true);
+            }
         }
         else
         {
